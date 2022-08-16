635e617ebc2785e80bae185b256ffe4b5c60354e
ztwelve <510625795@qq.com>
2022-08-16 18:53:19 +0800

    install sass

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,10 +1,13 @@
 <template>
-<div>
-    index page
-</div>
+    <div>
+        index page
+    </div>
 </template>
 <script>
 export default {
 
 }
 </script>
+<style lang="scss" scoped>
+
+</style>
